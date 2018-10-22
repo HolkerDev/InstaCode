66227a87f5b0d35075fd3bd077eab2eba059f99e
Nikolai Penkovskyi <nick.penkovskiy@gmail.com>
2018-10-22 11:33:56 +0200

    comments

--- app/src/main/java/com/example/holker/instacode/MainActivityKt.kt
+++ app/src/main/java/com/example/holker/instacode/MainActivityKt.kt
@@ -5,15 +5,24 @@ package com.example.holker.instacode
 import android.content.Context
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.support.v4.content.ContextCompat.getSystemService
 import android.support.v7.app.AppCompatActivity
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.view.inputmethod.InputMethodManager
+import com.example.holker.instacode.R.id.*
+import com.parse.LogInCallback
+import com.parse.ParseUser
 import kotlinx.android.synthetic.main.activity_main_kt.*
 
 class MainActivityKt : AppCompatActivity() {
 
-    var mLoginMode: Boolean = true
+    private var mLoginMode: Boolean = true
+
+    fun logIn() {
+        ParseUser.logInInBackground(username.text.toString(), password.text.toString(),
+        })
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +33,7 @@ class MainActivityKt : AppCompatActivity() {
         animation.setExitFadeDuration(3500)
         animation.start()
 
+        //change type of logging
         tv_switch.setOnClickListener {
             if (mLoginMode) {
                 btn_center.text = getString(R.string.signUp)
@@ -36,14 +46,16 @@ class MainActivityKt : AppCompatActivity() {
             }
         }
 
+        //LogIn
         btn_center.setOnClickListener {
             val animation: Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.bounce)
             btn_center.startAnimation(animation)
         }
 
+        //hide keyboard
         rl_main.setOnClickListener {
             val inputMethod = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            inputMethod.hideSoftInputFromWindow(this.currentFocus.windowToken,0)
+            inputMethod.hideSoftInputFromWindow(this.currentFocus.windowToken, 0)
         }
     }
 }
