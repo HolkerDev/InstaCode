a37b694d38bb3c4f69cbeb43ecb08c5e643a8a37
HolkerDev <nick.penkovskiy@gmail.com>
2018-11-07 22:52:01 +0100

    Add feed for user

--- app/src/main/java/com/example/holker/instacode/ContactsFragment.kt
+++ app/src/main/java/com/example/holker/instacode/ContactsFragment.kt
@@ -1,11 +1,13 @@
 package com.example.holker.instacode
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.parse.ParseQuery
@@ -13,13 +15,24 @@ import com.parse.ParseUser
 import kotlinx.android.synthetic.main.contacts_fragment.*
 
 class ContactsFragment : Fragment() {
+    val users = arrayListOf<String>()
 
     fun Context.toast(message: CharSequence) =
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
+    override fun onStart() {
+        super.onStart()
+        lv_userlist.setOnItemClickListener { parent: AdapterView<*>?, view: View?, position: Int, id: Long ->
+            kotlin.run {
+                val intent = Intent(context!!.applicationContext, Feed::class.java)
+                intent.putExtra("username", users[position])
+                startActivity(intent)
+            }
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        val users = arrayListOf<String>()
         val arrayAdapterUser = ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, users)
 
         val query: ParseQuery<ParseUser> = ParseUser.getQuery()
